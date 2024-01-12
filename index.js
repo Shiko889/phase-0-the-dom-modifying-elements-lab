663122a158b550d14d35625760a68b50192fa0fc
shiko <sarah.tharao@student.moringaschool.com>
2024-01-12 21:18:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Write your code here!// index.js
+const mainElement = document.getElementById('main');
+
+if (mainElement) {
+  mainElement.remove();
+}
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.body.appendChild(newHeader);
+
